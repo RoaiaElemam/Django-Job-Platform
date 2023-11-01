91e1d9eead22da31f82a76ef407aa622ecbbacce
RoaiaElemam <mroaia219@gmail.com>
2023-11-01 20:30:52 +0200

    customize jobs in admin

--- job/admin.py
+++ job/admin.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 from .models import Category, Job, Company
 # Register your models here.
 
+class JobAdmin(admin.ModelAdmin):
+    list_display=['title','location','company','job_type','vacancy','category']
+    search_fields=('title','category','description')
+    list_filter=('vacancy','category','job_type','experince')
 
-admin.site.register(Job)
+
+
+admin.site.register(Job,JobAdmin)
 admin.site.register(Company)
 admin.site.register(Category)
 

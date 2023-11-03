1b35a8da39e36c4e72464730e5217ef21b28de78
RoaiaElemam <mroaia219@gmail.com>
2023-11-03 16:38:07 +0200

    add job slug

--- job/models.py
+++ job/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django_countries.fields import CountryField
 from django.utils import timezone
+from django.utils.text import slugify
 
 JOB_TYPE = (
     ('full time','full time') ,#('for data base' , 'for user')
@@ -22,9 +23,15 @@ class Job (models.Model):
     job_type=models.CharField(choices=JOB_TYPE,max_length=10)
     experince=models.IntegerField()
     category=models.ForeignKey('Category',on_delete=models.SET_NULL,null=True,blank=True,related_name='job_category')
-    
+    slug=models.SlugField(null=True,blank=True)
+
+
     def __str__(self):
       return self.title
+    
+    def save(self, *args, **kwargs):
+     self.slug= slugify(self.title)
+     super(Job, self).save(*args, **kwargs) # Call the real save() method
 
 
 class Category (models.Model):

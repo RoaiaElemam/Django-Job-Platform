5a6230ea118c3674730aaa15c838203f6dadd65d
RoaiaElemam <mroaia219@gmail.com>
2023-11-03 17:33:07 +0200

    add detail

--- job/urls.py
+++ job/urls.py
@@ -1,7 +1,9 @@
-from .views import job_list
+from .views import job_list,job_detail
 from django.urls import path
 
 
 urlpatterns = [
     path('', job_list),
+    path('<slug:slug>', job_detail)
+
 ]
--- job/views.py
+++ job/views.py
@@ -4,3 +4,8 @@ from .models import Job
 def job_list(request):
     all_jobs=Job.objects.all()
     return render(request,'job/job_list.html',{'jobs':all_jobs})
+
+
+def job_detail(request,slug):
+    job=Job.objects.get(slug=slug)
+    return render(request,'job/job_detail.html',{'job':job})

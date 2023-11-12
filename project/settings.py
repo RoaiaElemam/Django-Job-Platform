fba5d3c0277b5e2724957efbf41385566d8879c2
RoaiaElemam <mroaia219@gmail.com>
2023-11-12 17:39:11 +0200

    add pagination

--- project/settings.py
+++ project/settings.py
@@ -45,6 +45,10 @@ INSTALLED_APPS = [
     #my apps
     'job',
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
